6c09ac6878563e15125b1e6a6f3f930b18c3737e
10dimpy <50054336+10dimpy@users.noreply.github.com>
2020-07-19 10:52:28 -0500

    Update register.jsx
    validations

--- components/register.jsx
+++ components/register.jsx
@@ -1,13 +1,70 @@
 import React from "react";
 import loginImg from "../login.svg";
+const validEmailRegex = RegExp(
+  /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
+);
+const validateForm = errors => {
+  let valid = true;
+  Object.values(errors).forEach(val => val.length > 0 && (valid = false));
+  return valid;
+};
 
-export class Register extends React.Component {
+export  class  Register extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      fullName: null,
+      email: null,
+      password: null,
+      errors: {
+        fullName: '',
+        email: '',
+        password: '',
+      }
+    };
   }
 
-  render() {
+  handleChange = (event) => {
+    event.preventDefault();
+    const { name, value } = event.target;
+    let errors = this.state.errors;
 
+    switch (name) {
+      case 'fullName':
+        errors.fullName =
+          value.length < 5
+            ? 'Full Name must be at least 5 characters long!'
+            : '';
+        break;
+      case 'email':
+        errors.email =
+          validEmailRegex.test(value)
+            ? ''
+            : 'Email is not valid!';
+        break;
+      case 'password':
+        errors.password =
+          value.length < 8
+            ? 'Password must be at least 8 characters long!'
+            : '';
+        break;
+      default:
+        break;
+    }
+
+    this.setState({errors, [name]: value});
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    if(validateForm(this.state.errors)) {
+      console.info('Valid Form')
+    }else{
+      console.error('Invalid Form')
+    }
+  }
+  render() {
+  const {errors} = this.state;
     return (
       <div className="base-container" ref={this.props.containerRef}>
         <div className="header">Register</div>
@@ -16,22 +73,35 @@ export class Register extends React.Component {
               <img src={loginImg} />
           </div>
           <div className="form">
-            <div className="form-group">
-              <label htmlFor="username">Username</label>
-              <input type="text" name="username" placeholder="username" />
+
+            <form onSubmit={this.handleSubmit} noValidate>
+             <div className="form-group">
+              <label htmlFor="fullName">Username</label>
+              <br></br>
+              <input type="text" name="fullName" placeholder="username" onChange={this.handleChange} noValidate />
+              {errors.fullName.length > 0 &&
+                <span className='error'>{errors.fullName}</span>}
             </div>
+
             <div className="form-group">
               <label htmlFor="email">Email</label>
-              <input type="text" name="email" placeholder="email" />
+              <input type="text" name="email" placeholder="email" onChange={this.handleChange} noValidate />
+              {errors.email.length > 0 &&
+                <span className='error'>{errors.email}</span>}
             </div>
             <div className="form-group">
               <label htmlFor="password">Password</label>
-              <input type="text" name="password" placeholder="password" />
+              <input type="password" name="password" placeholder="password"  onChange={this.handleChange} noValidate />
+              {errors.password.length > 0 &&
+                <span className='error'>{errors.password}</span>}
             </div>
              <div className="form-group">
               <label htmlFor="Confirm Password">Confirm Password</label>
-              <input type="text" name="Confirm Password" placeholder="Confirm password" />
+              <input type="password" name="Confirm Password" placeholder="Confirm password"onChange={this.handleChange} noValidate />
+              {errors.password.length > 0 &&
+                <span className='error'>{errors.password}</span>}
             </div>
+            </form>
           </div>
         </div>
         <div className="footer">
